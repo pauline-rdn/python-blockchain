26129390292d968ec0d1407270b6e97764eb7361
pauline rondeau <paulinerondeau@mbp-de-pauline.home>
2022-09-29 14:30:27 +0200

    most of work done

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import logo from '../assets/logo-pychain.png';
 import {API_BASE_URL} from '../config';
-import Blockchain from "./Blockchain";
+import {Link} from 'react-router-dom';
 
 function App() {
   const [walletInfo, setWalletInfo] = useState({});
@@ -19,12 +19,19 @@ function App() {
       <img className="logo" src={logo} alt="logo-pychain" />
       <h3>Welcome to pychain</h3>
       <br />
+      <Link to="/blockchain">Blockchain</Link>
+      <Link to="/conduct-transaction">Conduct a transaction</Link>
+      <Link to="/transaction-pool">Transaction Pool</Link>
       <div className="WalletInfo">
         <div>Address: {address}</div>
         <div>Balance: {balance}</div>
       </div>
       <br />
-      <Blockchain />
+      {/* 
+          <Blockchain />
+          <br />
+          <ConductTransaction /> 
+      */}
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {Router, Switch, Route} from 'react-router-dom';
+import history from './history'
 import './index.css';
 import App from './components/App';
+import Blockchain from './components/Blockchain';
+import ConductTransaction from './components/ConductTransaction';
+import TransactionPool from './components/TransactionPool';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Router history={history}>
+      <Switch>
+        <Route path='/' exact component={App} />
+        <Route path='/blockchain' component={Blockchain} />
+        <Route path='/conduct-transaction' component={ConductTransaction} />
+        <Route path='/transaction-pool' component={TransactionPool} />
+      </Switch>
+    </Router>
   </React.StrictMode>
 );
+
